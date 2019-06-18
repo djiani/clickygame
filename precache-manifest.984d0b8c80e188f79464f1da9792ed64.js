59c9f5f46a7d0dfa665d03eaef2beff564826327
Uncle Sam <djiasara@gmail.com>
2019-06-18 15:51:26 -0400

    Updates

---
+++ precache-manifest.984d0b8c80e188f79464f1da9792ed64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9db75294e1925e36a727df6ec84f3990",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "367ce195d394664a927a",
+    "url": "/clickygame/static/css/main.f71ccb1f.chunk.css"
+  },
+  {
+    "revision": "6c3e79be496f84b925c3",
+    "url": "/clickygame/static/js/2.d7f2626c.chunk.js"
+  },
+  {
+    "revision": "367ce195d394664a927a",
+    "url": "/clickygame/static/js/main.6bfc00e8.chunk.js"
+  },
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  }
+]);
